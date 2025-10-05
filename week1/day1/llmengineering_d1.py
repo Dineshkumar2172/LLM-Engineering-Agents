63d8e7982caf43564e1babec15a85e6ae6f0735f
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-10-05 16:56:49 +0530

    llm webpage summarizer

--- week1/day1/llmengineering_d1.py
+++ week1/day1/llmengineering_d1.py
@@ -2,25 +2,48 @@ import json
 import os
 import requests
 
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from IPython.display import Markdown, display
+from openai import OpenAI
 
 load_dotenv()
 
-headers = {
-    "Authorization": f"Bearer {os.getenv('SAGE_TOKEN')}",
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-}
+api_key = os.getenv("OPENAI_API_KEY")
 
-data = {
-    "messages": [
-        {
-            "content": "What is the meaning of life?",
-            "role": "user"
-        }
-    ],
-    "model": "gpt-4o-mini"
-}
+if not api_key: raise ValueError("SAGE_TOKEN environment variable not set")
 
-response = requests.post(os.getenv("SAGE_URL"), headers=headers, json=data)
-print(json.dumps(response.json(), indent=2))
+# headers = {
+#     "Authorization": f"Bearer {os.getenv('SAGE_TOKEN')}",
+#     "Content-Type": "application/json",
+#     "Accept": "application/json"
+# }
+
+# data = {
+#     "messages": [
+#         {
+#             "content": "What is the meaning of life?",
+#             "role": "user"
+#         }
+#     ],
+#     "model": "gpt-4o-mini"
+# }
+
+# response = requests.post(os.getenv("SAGE_URL"), headers=headers, json=data)
+# print(json.dumps(response.json(), indent=2))
+
+openai = OpenAI()
+
+class Website:
+    def __init__(self, url):
+        self.url = url
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.title = soup.title.string if soup.title else "No title found"
+        for irrelevant in soup.body(["script", "style", "img", "input"]):
+            irrelevant.decompose()
+        self.text = soup.body.get_text(separator="\n", strip=True)
+        
+ed = Website("https://edwarddonner.com")
+print(ed.text)
+
