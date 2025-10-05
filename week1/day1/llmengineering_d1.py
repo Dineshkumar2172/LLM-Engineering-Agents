363ef7d40f8fcb9a0cc19e49d52161244504abf6
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-10-05 17:49:11 +0530

    sage ai tailored

--- week1/day1/llmengineering_d1.py
+++ week1/day1/llmengineering_d1.py
@@ -13,27 +13,7 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 if not api_key: raise ValueError("SAGE_TOKEN environment variable not set")
 
-# headers = {
-#     "Authorization": f"Bearer {os.getenv('SAGE_TOKEN')}",
-#     "Content-Type": "application/json",
-#     "Accept": "application/json"
-# }
-
-# data = {
-#     "messages": [
-#         {
-#             "content": "What is the meaning of life?",
-#             "role": "user"
-#         }
-#     ],
-#     "model": "gpt-4o-mini"
-# }
-
-# response = requests.post(os.getenv("SAGE_URL"), headers=headers, json=data)
-# print(json.dumps(response.json(), indent=2))
-
 openai = OpenAI()
-
 class Website:
     def __init__(self, url):
         self.url = url
@@ -45,8 +25,8 @@ class Website:
         self.text = soup.body.get_text(separator="\n", strip=True)
         
 ed = Website("https://edwarddonner.com")
-print(ed.title)
-print(ed.text)
+# print(ed.title)
+# print(ed.text)
 
 # Types of prompts
 #       1. system prompt - that tells them what task they are performing and what tone they should use.
@@ -86,4 +66,41 @@ def messages_for(website):
         {"role": "user", "content": user_prompt_for(website)}
     ]
 
+# # and now: call the OpenAI API.
+# def summarize(url):
+#     website = Website(url)
+#     response = openai.chat.completions.create(
+#         model = "gpt-40-mini",
+#         messages = messages_for(website)
+#     )
+#     return response.choices[0].message.content
 
+# summarize("https://edwarddonner.com")
+
+# def display_summary(url):
+#     summary = summarize(url)
+#     display(Markdown(summary))
+
+# display_summary("https://edwarddonner.com")
+
+# tailoring it to support our sage API instead of OpenAI import - use to get response from our sage
+def sage_ai(messages, model):
+    headers = {
+        "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}",
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+    data = { "messages": messages, "model": model }
+    response = requests.post(os.getenv("SAGE_URL"), headers=headers, json=data)
+    return response.json()
+
+def summarize(url):
+    website = Website(url)
+    response = sage_ai( model = "gpt-4o-mini", messages = messages_for(website) )    
+    return response["choices"][0]["message"]["content"]
+
+def display_summary(url):
+    summary = summarize(url)
+    display(Markdown(summary))
+
+display_summary("https://edwarddonner.com")
