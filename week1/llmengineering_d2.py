3919d2652fa97ed80d2add59f88975acc094bb45
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-10-05 18:29:36 +0530

    day 2 - using ollama

--- week1/llmengineering_d2.py
+++ week1/llmengineering_d2.py
@@ -20,6 +20,7 @@
 #     Phi from Microsoft
 #
 # Three ways to use models
+#
 #     1. Chat interfaces like ChatGPT, Claude, Bard
 #     2. Cloud APIs - LLM API (accessing through code instead of chat interface - per API request cost)
 #        Frameworks like Langchain - provides unified interface to access multiple models
@@ -27,3 +28,5 @@
 #     3. Direct inference - downloading model weights and running inference on local machine or cloud VM
 #        With the HuggingFace transformers library, you can run inference on many open-source models
 #        With Ollama to run locally
+
+
